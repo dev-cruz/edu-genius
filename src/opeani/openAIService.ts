9236b23b0ca7302c9525b16507e40d129003bc42
Henrique Freitas <henrique.manoel@hotmail.com.br>
2023-08-19 16:41:58 -0300

    generate questions

--- src/opeani/openAIService.ts
+++ src/opeani/openAIService.ts
@@ -5,14 +5,18 @@ import { OpenAIConfig } from './openAIConfig';
 export class OpenAIService {
   constructor(private readonly openai: OpenAIConfig) {}
     
-  async prompt(): Promise<any> {  
+  async generateQuestions(level:string, subject:string, questions: number): Promise<any> {  
     const completion = await this.openai.instance().chat.completions.create({
-      messages: [{ role: 'user', content: 'Say this is a test' }],
+      messages: [{ role: 'user', content: this.prompt(level, subject, questions)}],
       model: 'gpt-3.5-turbo',
     });
 
     console.log(completion.choices);
 
     return completion.choices;
+  }
+
+  private prompt(level:string, subject:string, questions: number): string {
+    return `Gere ${questions} questões de múltipla escolha do nível ${level} da disciplina ${subject}`
   }
 }
