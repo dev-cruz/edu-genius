4a5f219fa092be43b117c40cf8a79080e884563e
Henrique Freitas <henrique.manoel@hotmail.com.br>
2023-08-19 16:06:14 -0300

    add opeai service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OpenAIService } from './opeani/openAIService';
+import { OpenAIConfig } from './opeani/openAIConfig';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, OpenAIService, OpenAIConfig],
 })
 export class AppModule {}
---
+++ src/opeani/openAIConfig.ts
@@ -0,0 +1,12 @@
+import OpenAI from 'openai';
+
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class OpenAIConfig {
+  instance(): OpenAI {
+    return new OpenAI({
+      apiKey: process.env["OPENAI_API_KEY"],
+    });
+  }
+}
