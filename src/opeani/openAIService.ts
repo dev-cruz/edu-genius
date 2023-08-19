6fb9d96993effc03e6cb7020e9eb88e90f5381bd
Henrique Freitas <henrique.manoel@hotmail.com.br>
2023-08-19 16:29:14 -0300

    fix opeai

--- src/opeani/openAIService.ts
+++ src/opeani/openAIService.ts
@@ -1,13 +1,12 @@
-import OpenAI from 'openai';
-
 import { Injectable } from '@nestjs/common';
+import { OpenAIConfig } from './openAIConfig';
 
 @Injectable()
 export class OpenAIService {
-  constructor(private readonly openai: OpenAI) {}
+  constructor(private readonly openai: OpenAIConfig) {}
     
   async prompt(): Promise<any> {  
-    const completion = await this.openai.chat.completions.create({
+    const completion = await this.openai.instance().chat.completions.create({
       messages: [{ role: 'user', content: 'Say this is a test' }],
       model: 'gpt-3.5-turbo',
     });
