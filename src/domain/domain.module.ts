9e12d931181241219cc1673a14861505347ee639
ramon-passos <ramonpassos2011@gmail.com>
2023-08-19 21:02:27 -0300

    import inframodule on domain

--- src/domain/domain.module.ts
+++ src/domain/domain.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ContentGeneratorService } from 'src/domain/services/contentGeneratorService';
+import { InfraModule } from 'src/infra/infra.module';
 
 @Module({
-  imports: [],
+  imports: [InfraModule],
   controllers: [],
   providers: [ContentGeneratorService],
 })
-export class DomainModule { }
+export class DomainModule {}
